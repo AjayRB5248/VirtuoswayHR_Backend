6cdaaaa480d85f2a1eb7e6534b3cab145fd046f9
Ajay Ranabhat <47922566+arb12345@users.noreply.github.com>
2023-01-25 20:28:40 +0545

    Update applicantController.js

--- controller/applicantController.js
+++ controller/applicantController.js
@@ -1,76 +1,84 @@
 const applicantModel = require("../model/Applicant");
-const cloudinary = require("cloudinary").v2;
-// const multer = require("multer");
-// const path = require("path");
-// const fs = require("fs");
+// const cloudinary = require("cloudinary").v2;
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     const uploadPath = path.resolve(__dirname, "../upload");
-//     if (!fs.existsSync(uploadPath)) {
-//       fs.mkdirSync(uploadPath);
-//     }
-//     cb(null, uploadPath);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `resume-${Date.now()}.pdf`);
-//   },
-// });
-// const upload = multer({
-//   storage,
-//   fileFilter: (req, file, cb) => {
-//     if (file.mimetype === "application/pdf") {
-//       cb(null, true);
-//     } else {
-//       cb(new Error("Invalid file type, only PDF is allowed!"), false);
-//     }
-//   },
+
+// cloudinary.config({
+//   cloud_name: "dhupp7wwx",
+//   api_key: "549775959516217",
+//   api_secret: "48zYFHE8fpoDdBgdDQuXSMCZd44",
 // });
 
-cloudinary.config({
-  cloud_name: "dhupp7wwx",
-  api_key: "549775959516217",
-  api_secret: "48zYFHE8fpoDdBgdDQuXSMCZd44",
-});
-
+// const createApplicant = async (req, res) => {
+//   cloudinary.uploader.upload(req.file.path, (error, result) => {
+//     if (error) {
+//       return res.status(500).send(error);
+//     }
+//     const {
+//       firstName,
+//       lastName,
+//       email,
+//       phone,
+//       address,
+//       city,
+//       state,
+//       postal,
+//       github,
+//     } = req.body;
+//     const applicant = new applicantModel({
+//       firstName,
+//       lastName,
+//       email,
+//       phone,
+//       address,
+//       city,
+//       state,
+//       postal,
+//       github,
+//       resume: result.url,
+//     });
+//     try {
+//       applicant.save();
+//       res
+//         .status(201)
+//         .json({ message: "Applicant Created Succesfully", applicant });
+//     } catch (e) {
+//       res.status(400).json({ message: "Something Went Wrong", e });
+//     }
+//   });
+// };
 const createApplicant = async (req, res) => {
-  cloudinary.uploader.upload(req.file.path, (error, result) => {
-    if (error) {
-      return res.status(500).send(error);
-    }
-    const {
-      firstName,
-      lastName,
-      email,
-      phone,
-      address,
-      city,
-      state,
-      postal,
-      github,
-    } = req.body;
-    const applicant = new applicantModel({
-      firstName,
-      lastName,
-      email,
-      phone,
-      address,
-      city,
-      state,
-      postal,
-      github,
-      resume: result.url,
-    });
-    try {
-      applicant.save();
-      res
-        .status(201)
-        .json({ message: "Applicant Created Succesfully", applicant });
-    } catch (e) {
-      res.status(400).json({ message: "Something Went Wrong", e });
-    }
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    address,
+    city,
+    state,
+    postal,
+    github,
+  } = req.body;
+  const applicant = new applicantModel({
+    firstName,
+    lastName,
+    email,
+    phone,
+    address,
+    city,
+    state,
+    postal,
+    github,
+    resume: req.file.filename,
   });
-};
+  try {
+    applicant.save();
+    res
+      .status(201)
+      .json({ message: "Applicant Created Succesfully", applicant });
+  } catch (e) {
+    res.status(400).json({ message: "Something Went Wrong", e });
+  }
+}
 
 const getApllicant = async (req, res) => {
   try {
